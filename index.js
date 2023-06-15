ca6e50a9037b8004dcf3c908998487cffa3a3f11
Julia Bauer <juliabauer@ufl.edu>
2023-06-14 20:22:19 -0400

    commit

---
+++ index.js
@@ -0,0 +1,35 @@
+// Add to to-do element on click or enter with form 
+var form = document.querySelector('form')
+
+// Need array for tests to register multiple todos?
+// var tasks = [] ?
+
+// Save user-entered data
+form.onsubmit = function(e) {
+    e.preventDefault()
+    var input = document.querySelector('input')
+    console.log(input.value)
+    var task = input.value
+    // Cannot enter spaces
+    if(task.trim().length === 0) {
+        return
+    }  
+    // Clear submission line
+    form.reset()
+    // Make list
+    taskList = document.createElement('li')
+    taskButton = document.createElement('button')
+    var list = document.getElementById("todo-list")
+    list.appendChild(taskList)
+    taskList.appendChild(taskButton)
+    // Make bullet points show input
+    taskButton.textContent = task
+    // Strike through
+    taskButton.onclick = function() {
+        taskButton.style = 'text-decoration: line-through'
+        // Remove on second click
+        taskButton.onclick = function() {
+        taskList.remove()
+        }
+    }
+}
